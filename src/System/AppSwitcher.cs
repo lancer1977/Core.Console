dc587e9994a1dbcac9b9fa716af8ffbcc9a08f37
chris <lancer1977@gmail.com>
2024-10-26 23:26:21 -0400

    appswitchers

---
+++ src/System/AppSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace OAuth.Core.System;
+ 
+public class AppSwitcher
+{ // Import the SetForegroundWindow function from user32.dll
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetForegroundWindow(nint hWnd);
+    public static bool SwitchToApp(string appName)
+    {
+        var proc = Process.GetProcessesByName(appName).FirstOrDefault();
+        if (proc != null)
+        {
+            var handle = proc.MainWindowHandle;
+            SetForegroundWindow(handle);
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Process with the name '{0}' not found.", appName);
+            return false;
+        }
+    }
+
+
+}
